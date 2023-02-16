bb165bf57f05aeb3342c5d3b2ba6f4e133237158
Babavega <114047064+Babavega@users.noreply.github.com>
2023-02-16 09:54:40 +0530

    Create bb.js

---
+++ bb.js
@@ -0,0 +1,25 @@
+let titlea = "Part 1 Prescription";
+let titleb = "Part 2 Prescription";
+let titlec = "Part 3 Prescription";
+let titled = "Part 4 Prescription";
+let titlee = "Calculation based on Temperature";
+let titlef = "Isotonicity Calculations";
+let titleg = "Calculation of Density Mass and Volume";
+let titleh = "Posology";
+let titlei = "Alligation Method";
+let titlej = "Percentage Calculations";
+let titlek = "Volume of Distribution and Clearance";
+let titlel = "Proof Spirit";
+
+let vida = "J2bkiZYhlC8";
+let vidb = "jGiJXdMs0aM";
+let vidc = "N9QSxQlqFNM";
+let vidd = "KfDrHS-IVOU";
+let vide = "qebU3MVbIIE";
+let vidf = "vIcpPibu5_E";
+let vidg = "Kux4vK_JNhk";
+let vidh = "odC7x1cs1pQ";
+let vidi = "DAp7sDvLkuQ";
+let vidj = "ATMB7jB8jeQ";
+let vidk = "hZihF8xjS88";
+let vidl = "M27pcCOMy_A";
